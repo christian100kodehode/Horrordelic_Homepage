5e02b4c18cc038df59593dd65cbfe6719ba2d0e0
Christian Westbye <christian100.kodehode@gmail.com>
2023-03-21 11:33:40 +0100

    updates

--- src/components/Artists.jsx
+++ src/components/Artists.jsx
@@ -1,10 +1,44 @@
 import React from "react";
 import styles from "./Artists.module.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Artists = () => {
+  // State for loading
+  const [isLoading, setIsLoading] = useState(false);
+  // State for Release data
+  const [artist, setArtist] = useState([]);
+
+  const fetchData = () => {
+    setIsLoading(true);
+    setTimeout(
+      async () => {
+        const API_URL = `./artist-list.json`;
+        const response = await axios.get(API_URL);
+        setArtist(response.data);
+        setIsLoading(false);
+      }
+      // 2000 - if wanting timeout
+    );
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // console.log(artist);
   return (
     <div className={styles.container}>
-      <h1>Coming Soon ...</h1>
+      {artist.map(({ name, id }) => {
+        return (
+          <div key={id} className={styles.artistsContainer}>
+            <h2>
+              <Link to={`/artist/${name}`}>{name}</Link>
+            </h2>
+          </div>
+        );
+      })}
     </div>
   );
 };
