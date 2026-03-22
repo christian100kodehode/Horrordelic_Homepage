7c62a86bddf65d4dd68f674055b8839dffbf486d
Christian Westbye <101109255+christian100kodehode@users.noreply.github.com>
2026-03-22 05:55:35 +0100

    Test

--- api/ssrOG.js
+++ api/ssrOG.js
@@ -1,3 +1,3 @@
-htmlContent = htmlContent.replace(/__TITLE__/g, ogData.title);
-htmlContent = htmlContent.replace(/__DESCRIPTION__/g, ogData.description);
-htmlContent = htmlContent.replace(/__OG_TITLE__/g, ogData.ogTitle);
+htmlContent = htmlContent.replace(/__TITLE__/g, property);
+htmlContent = htmlContent.replace(/__DESCRIPTION__/g, description);
+htmlContent = htmlContent.replace(/__OG_TITLE__/g, ogTitle);
