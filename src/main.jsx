95365f0e42230fefdab9b7357b0be7d24547c9aa
christian100.kodehode <christian100.kodehode@gmail.com>
2026-08-02 23:15:57 +0200

    updates to headers in vercel.json

--- src/main.jsx
+++ src/main.jsx
@@ -4,46 +4,6 @@ import App from "./App";
 import { BrowserRouter } from "react-router";
 import { CookieManager } from "react-cookie-manager";
 
-// Force it to use nocookie
-if (typeof window !== "undefined") {
-  window.YTConfig = { host: "https://www.youtube-nocookie.com" };
-}
-
-//  <CookieManager  
-
-//            translations={{
-//           title: "Youtube and Google Cookies 🍪",
-//           message:
-//           "We value your privacy, but Youtube/Google will go ahead with their cookies. Accept to save and play as normal.",
-//           buttonText: "Accept Needed for video",
-        
-//       }} 
-//   showManageButton={false}
-//   enableFloatingButton={true}  
-// cookieCategories={{
-//     Analytics: true, // Show Analytics category
-//     Social: false, // Hide Social category
-//     Advertising: true, // Show Advertising category  
-//   }}
-//      initialPreferences={{Analytics: true,
-//   Social: false,
-//   Advertising:true}}
-
-// onManage={(preferences) => {
-//         if (preferences) {
-//           console.log("Cookie preferences updated:", preferences);
-//         }
-//       }}
-//       onAccept={() => {
-//         console.log("User accepted all cookies");
-
-//       }}
-//       onDecline={() => {
-//         console.log("User declined all cookies");
-//          window.gtag?.("consent", "update", { analytics_storage: "denied" });
-     
-//       }}     
-//       ></CookieManager>
 
 {/* // createRoot(document.getElementById("root")).render( */}
 ReactDOM.createRoot(document.getElementById("root")).render(
